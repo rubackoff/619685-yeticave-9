fd1bc7e7b54b6a32c2235bbbc5cbe976703d32ee
Сева <rubackoff@gmail.com>
2019-04-29 19:33:10 +0300

    Шаблонизация

--- templates/index.php
+++ templates/index.php
@@ -1,14 +1,5 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Главная</title>
-    <link href="../css/normalize.min.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
-</head>
-<body>
-<div class="page-wrapper">
-
     <main class="container">
         <section class="promo">
             <h2 class="promo__title">Нужен стафф для катки?</h2>
@@ -108,9 +99,4 @@
             </ul>
         </section>
     </main>
-</div>
-
-<script src="flatpickr.js"></script>
-<script src="script.js"></script>
-</body>
 </html>
